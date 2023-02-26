444ba0dd11cfbbaeffd4f29787aa043f0869ccef
Ted <teddychenster@gmail.com>
2023-02-26 18:02:50 +0800

    Created temporary runable component for schedule template
    - model
    - agent
    
    The codes are just for temporary testing, so that the proceduer and
    steps of the model can be logged to check for correctness

--- model/src/agent.py
+++ model/src/agent.py
@@ -1,12 +1,29 @@
+from __future__ import annotations
 import mesa
 from typing import TYPE_CHECKING
+from enum import Enum, auto
+
+
+import logging
+logger = logging.getLogger('structure')
 
 if TYPE_CHECKING:
-    from model import CModel
+    from src.model import CModel
+
+class AgentType(Enum):
+    BUYER = auto()
+    SELLER = auto()
 
 class CAgent(mesa.Agent):
 
-    def __init__(self, model: CModel):
-        pass
-
+    def __init__(self, id:int, model: CModel, type: AgentType):
+        super().__init__(id, model)
+        self.y = 10 # need change
+        self.type : AgentType = type
+        
+    def decide_consumption(self, *args, **kargs) -> float:
+        logger.debug(f"Deciding consumptions for buyer {self.unique_id}")
+        budget = self.y * 0.5
+        return budget 
+    
     
--- model/src/model.py
+++ model/src/model.py
@@ -1,12 +1,20 @@
+from __future__ import annotations
+from random import Random
 import mesa
 import mesa.time
+from src.agent import CAgent, AgentType
+from src.schedule import CScheduler
+
+import logging
+logger = logging.getLogger('structure')
+
 from typing import (
     TYPE_CHECKING,
     Type
     )
 
 if TYPE_CHECKING:
-    SchedulerConstructor =  Type[mesa.time.BaseScheduler]
+    SchedulerConstructor =  Type[CScheduler]
 
 
 class CModel(mesa.Model):
@@ -14,7 +22,39 @@ class CModel(mesa.Model):
         N                       : int,
         scheduler_constructor   : SchedulerConstructor,
         ):
+        self.random: Random
+        super().__init__()
         self.scheduler = scheduler_constructor(self)
+        self.unique_id_generator = (i for i in range(100_00_00_00) )
+        self._create_agents()
+
+    
+    def _create_agents(self):
+        logger.debug("Creating agents")
+        self._init_buyers()
+        self._init_sellers()
+    
+    def _init_buyers(self):
+        for i in range(2):
+            self.scheduler.add(
+                CAgent(
+                    id = next(self.unique_id_generator),
+                    model=self,
+                    type = AgentType.BUYER
+                    ), 
+                type=AgentType.BUYER
+            )
+
+    def _init_sellers(self):
+        for i in range(2):
+            self.scheduler.add(
+                CAgent(
+                    id = next(self.unique_id_generator),
+                    model=self,
+                    type = AgentType.SELLER
+                    ), 
+                type=AgentType.SELLER
+            )
 
 
 
